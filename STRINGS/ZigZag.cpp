00f4af4aa1fedacedea17d9e2b092e9be7146fb1
Suyash <mishrasuyash013@gmail.com>
2025-02-22 22:10:17 +0530

    21 Feb

---
+++ STRINGS/ZigZag.cpp
@@ -0,0 +1,20 @@
+// Leetcode 6
+
+#include <iostream>
+using namespace std;
+int main()
+{
+
+
+
+    for(int i =0; i<5; i++){
+        for(int j= 0; j<5; j++){
+            cout << j <<" ";
+
+            if()
+        }
+        cout << endl;
+    }
+    cout << endl;
+    return 0;
+}
