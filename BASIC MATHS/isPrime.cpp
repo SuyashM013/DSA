d89429ba4975a6912622ab2a5adec046be29a0f1
Suyash <mishrasuyash013@gmail.com>
2025-03-05 00:46:17 +0530

    5 Mar

--- BASIC MATHS/isPrime.cpp
+++ BASIC MATHS/isPrime.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
 
 bool isPrime(int n){
+    // if(n == 1) return false;
+    // for(int i = 2; i<= n-1; i++){
+    //     if(n%1 == 0) return false;
+    // }
+    // return true;
+
     if(n == 1) return false;
-    for(int i = 2; i<= n-1; i++){
-        if(n%1 == 0) return false;
+    for(int i = 2; i<= sqrt(n); i++){
+        if(n%i == 0) return false;
     }
     return true;
 }
@@ -12,7 +19,7 @@ bool isPrime(int n){
 int main()
 {
 
-    cout << isPrime(10);
+    cout << isPrime(2);
     cout << endl;
     return 0;
 }
