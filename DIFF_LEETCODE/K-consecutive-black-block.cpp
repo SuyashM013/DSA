434acf84e942b6b9fcc1d2b69985d1c9707689b4
Suyash <mishrasuyash013@gmail.com>
2025-03-09 00:23:53 +0530

    9 Mar

---
+++ DIFF_LEETCODE/K-consecutive-black-block.cpp
@@ -0,0 +1,26 @@
+// leetcode - 2379 easy
+
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+// int n = blocks.size();
+// int black = 0;
+// int ans = INT_MAX;
+
+// for(int i = 0; i<n; ++i){
+//     if(blocks[i] == 'B') black++;
+//     if(i >= k-1){
+//         ans = min(ans, k-black);
+//         if(blocks[i-k+1] == 'B') black--;
+//     }
+// }
+// return ans;
+
+int main()
+{
+
+    cout << endl;
+    return 0;
+}
