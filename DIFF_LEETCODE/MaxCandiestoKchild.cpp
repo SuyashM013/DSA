4156828c58b76e9e8ecd27521cc1cf3197fc4280
Suyash <mishrasuyash013@gmail.com>
2025-03-14 23:18:11 +0530

    14 Mar

--- DIFF_LEETCODE/MaxCandiestoKchild.cpp
+++ DIFF_LEETCODE/MaxCandiestoKchild.cpp
@@ -8,8 +8,45 @@ using namespace std;
 
 class Solution {
     public:
-        int maximumCandies(vector<int>& candies, long long k) {
-            int n = candies.size();
+    bool canDistribute(vector<int>& candies, int mid, long long k) {
+        int n = candies.size();
+
+        for (int i = 0; i < n; i++) {
+            k -= candies[i] / mid;
+
+            if (k <= 0) {
+                return true;
+            }
+        }
+        return k <= 0;
+    }
+
+    int maximumCandies(vector<int>& candies, long long k) {
+        int n = candies.size();
+        long long total = 0;
+        int mac = 0;
+        for (int i = 0; i < n; i++) {
+            mac = max(mac, candies[i]);
+            total += candies[i];
+        }
+        if (total < k) {
+            return 0;
+        }
+
+        int left = 1;
+        int right = mac;
+        int result = 0;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (canDistribute(candies, mid, k)) {
+                result = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return result;
     
             // Brute Force
     
