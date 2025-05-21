71c4ee6fe139a094b01256387e4f48ff33159d00
Suyash <mishrasuyash013@gmail.com>
2025-05-22 00:34:18 +0530

    22 May

---
+++ STACKS-/ValidParentheses.cpp
@@ -0,0 +1,45 @@
+// leetcode - 20
+#include <iostream>
+#include <stack>
+using namespace std;
+
+
+class Solution {
+public:
+    bool isValid(string s) {
+        
+        if (s.length() % 2 != 0) {
+            return false;
+
+        }
+        stack<char> st;
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
+                st.push(s[i]);
+            } else {
+                if (st.size() == 0) {
+                     return false;
+                }
+                if ((st.top() == '(' && s[i] == ')') ||
+                    (st.top() == '{' && s[i] == '}') ||
+                    (st.top() == '[' && s[i] == ']')) {
+                    st.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        if (st.size() == 0)
+            return true;
+        else
+            return false;
+
+        
+    }
+};
+
+int main()
+{
+    cout << endl;
+    return 0;
+}
