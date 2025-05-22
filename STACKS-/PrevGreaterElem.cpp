96e1de394f550881c60f494b91c99b734104da31
Suyash <mishrasuyash013@gmail.com>
2025-05-23 01:28:03 +0530

    23 May

--- STACKS-/PrevGreaterElem.cpp
+++ STACKS-/PrevGreaterElem.cpp
@@ -1,20 +1,22 @@
 #include <iostream>
 #include <stack>
-
 using namespace std;
+
 
 int main()
 {
+
+    
     int arr[] = {3,1,2,5,4,6,2,3};
     int n = sizeof(arr) / sizeof(arr[0]);
 
     int nge[n];
 
     stack<int> st;   // pop, ans, push
-    nge[n - 1] = -1;
-    st.push(arr[n-1]);
+    nge[0] = -1;
+    st.push(arr[0]);
 
-    for(int i = n-2; i>= 0; i--){
+    for(int i = 1; i<n; i++){
         while(st.size() > 0 && st.top() <= arr[i]){
             st.pop();
         }
