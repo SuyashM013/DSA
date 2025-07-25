f1628c59b1a14414b34c393e47ddef541dbf3f3a
Suyash <mishrasuyash013@gmail.com>
2025-07-25 21:02:27 +0530

    25 Jul

--- RECURSION/pip.cpp
+++ RECURSION/pip.cpp
@@ -6,11 +6,11 @@ void pip(int a)
     if (a == 0)
         return;
 
-    cout << "Pre" << a << endl;
+    cout << "Pre " << a << endl;
     pip(a - 1);
-    cout << "In" << a << endl;
+    cout << "In " << a << endl;
     pip(a - 1);
-    cout << "Post" << a << endl;
+    cout << "Post " << a << endl;
 }
 
 int main()
