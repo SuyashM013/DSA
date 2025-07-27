8f5f6f585101630380f36bc47573da58353332a8
Suyash <mishrasuyash013@gmail.com>
2025-07-28 01:04:45 +0530

    28 Jul

--- BINARY TREES/DFS_Traversal.cpp
+++ BINARY TREES/DFS_Traversal.cpp
@@ -1,58 +1,89 @@
 #include <iostream>
 using namespace std;
 
-class Node{
-    public:
+class Node
+{
+public:
     int val;
-    Node* left;
-    Node* right;
-    Node(int val){
+    Node *left;
+    Node *right;
+    Node(int val)
+    {
         this->val = val;
         this->left = NULL;
         this->right = NULL;
     }
 };
 
-void preorder(Node* root){
+void preorder(Node *root)
+{
 
-    if(root == NULL) return;
+    if (root == NULL)
+        return;
     cout << root->val << " ";
     preorder(root->left);
     preorder(root->right);
-
 }
-void inorder(Node* root){
+void inorder(Node *root)
+{
 
-    if(root == NULL) return;
+    if (root == NULL)
+        return;
     inorder(root->left);
     cout << root->val << " ";
     inorder(root->right);
-
 }
 
-void postorder(Node* root){
+void postorder(Node *root)
+{
 
-    if(root == NULL) return;
+    if (root == NULL)
+        return;
     inorder(root->left);
     inorder(root->right);
     cout << root->val << " ";
+}
 
+void nthlevel(Node *root, int n, int level)
+{
+    if (root == NULL)
+        return;
+    if (n == level)
+    {
+
+        cout << root->val << " ";
+        return;
+    }
+    nthlevel(root->right, n + 1, level);
+    nthlevel(root->left, n + 1, level);
+}
+int levels(Node* root){
+     if(root==NULL) return 0;
+    return 1 + max(levels(root->left), levels(root->right));
+}
+
+void levelorder(Node* root){
+    int n = levels(root);
+    for(int i = 1; i<=n; i++){
+        nthlevel(root, 1, i);
+        cout << endl;
+    }
 }
 
 int main()
 {
 
-    Node* a = new Node(1);
-    Node* b = new Node(2);
-    Node* c = new Node(3);
-    Node* d = new Node(4);
-    Node* e = new Node(5);
-    Node* f = new Node(6);
-    Node* g = new Node(7);
-    Node* h = new Node(8);
-    Node* i = new Node(9);
-    Node* j = new Node(10);
-    Node* k = new Node(11);
+    Node *a = new Node(1);
+    Node *b = new Node(2);
+    Node *c = new Node(3);
+    Node *d = new Node(4);
+    Node *e = new Node(5);
+    Node *f = new Node(6);
+    Node *g = new Node(7);
+    Node *h = new Node(8);
+    Node *i = new Node(9);
+    Node *j = new Node(10);
+    Node *k = new Node(11);
 
     a->left = b;
     a->right = c;
@@ -72,7 +103,11 @@ int main()
     postorder(a);
     cout << endl;
 
- 
+    nthlevel(a,1,3);
+    cout << endl;
+
+    levelorder(a);
+
     cout << endl;
     return 0;
 }
