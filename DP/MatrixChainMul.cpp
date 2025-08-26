f1adab0e325e693e4044a059be8433af3e248ff1
Suyash <mishrasuyash013@gmail.com>
2025-08-26 23:52:30 +0530

    26 Aug

---
+++ DP/MatrixChainMul.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+using namespace std;
+
+int cost(vector<int>& m, int i, int j, vector<vector<int>>& dp){
+    if(i == j || i+1 == j) return 0;
+    if (dp[i][j] != -1) return dp[i][j];
+    int ans = INT_MAX;
+
+    for(int k = i+1; k<j; k++){
+        ans = min({ans,cost(m,i,k,dp)+ cost(m,k,j,dp) +m[i]* m[j]* m[k] });
+    }
+    return dp[i][j] = ans;
+
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> m;
+    vector<vector<int>> dp(10000, vector<int>(10000, -1));
+    for(int i = 0; i<n ; i++){
+        int tem;
+        cin >> tem;
+        m.push_back(tem);
+    }
+    // for(int i =0 ; i<n; i++){
+    //     cout << m[i]<<" ";
+    // }
+    cout << cost(m,0,n-1,dp);
+
+
+
+    cout << endl;
+    return 0;
+}
